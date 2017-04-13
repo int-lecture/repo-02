d4e77c846d040e160634a917a19c95e9cfd6e00f
Murmelmunz <munzinger3@gmail.com>
2017-04-13 11:50:15 +0200

    Bugfix

--- Homepage/04 Version 1.0.3/Chat/chat.php
+++ Homepage/04 Version 1.0.3/Chat/chat.php
@@ -6,7 +6,7 @@
 	$datei = "chats/" . $to . "/" . "nachrichten.txt";
 	$datei2 = "chats/" . $from . "/" . "nachrichten.txt";
 	file_put_contents ($datei, $from . " schrieb um " . date("h:i:sa") . ": " . $message . "<hr>\r\n",FILE_APPEND);
-	file_put_contents ($datei2, $from . " schrieb um " . date("h:i:sa") . ": " . $message . "<hr>\r\n",FILE_APPEND);
+	file_put_contents ($datei2, $from . " schrieb an " . $to . " um " . date("h:i:sa") . ": " . $message . "<hr>\r\n",FILE_APPEND);
 	fclose($datei2);
 	fclose($datei);
 	echo "<br /><a href=\"messenger.php\">Rückkehr zum Messenger, falls automatische Umleitung nach 3 Sekunden nicht funktioniert hat</a>";
