ee2fefa931949a820366a079e5e9a0e1141f4bd7
Murmelmunz <munzinger3@gmail.com>
2017-04-13 11:37:16 +0200

    Mehr Funktionen hinzugefügt
    Automatisch Umleitung, Zeitanzeige

---
+++ Homepage/04 Version 1.0.3/Chat/chat.php
@@ -0,0 +1,14 @@
+<?php 
+	$from = $_COOKIE["benutzer"];
+	$to = $_POST["empfaenger"];
+	$message = $_POST["nachricht"];
+	echo "Nachricht von " . $from . " an " . $to . "-->  " . $message . " wurde versandt.";
+	$datei = "chats/" . $to . "/" . "nachrichten.txt";
+	$datei2 = "chats/" . $from . "/" . "nachrichten.txt";
+	file_put_contents ($datei, $from . " schrieb um " . date("h:i:sa") . ": " . $message . "<hr>\r\n",FILE_APPEND);
+	file_put_contents ($datei2, $from . " schrieb um " . date("h:i:sa") . ": " . $message . "<hr>\r\n",FILE_APPEND);
+	fclose($datei2);
+	fclose($datei);
+	echo "<br /><a href=\"messenger.php\">Rückkehr zum Messenger, falls automatische Umleitung nach 3 Sekunden nicht funktioniert hat</a>";
+	header("location: messenger.php");
+?>
