8587be1d5c3165fd5c1013c667d02fe485f1396e
Philip Oesterlin <1612340@stud.hs-mannheim.de>
2017-05-03 17:33:45 +0200

    get aufruf gibt nachrichten zurück und kann löschen

--- ChatServer/src/var/Messages.java
+++ ChatServer/src/var/Messages.java
@@ -15,8 +15,19 @@ import org.codehaus.jettison.json.JSONObject;
 @Path("/messages/{user_id}/{sequenceNumber}")
 @Produces(MediaType.APPLICATION_JSON)
 public class Messages {
-
+	LinkedList<String[]> ll = new LinkedList<>();
 	int seqCounter = 0;
+
+	{
+	String[] s = {"bob","David","hallo David",seqCounter++ + ""};
+	ll.addFirst(s);
+	String[] s2 = {"bob","David","hallo David die zweite",seqCounter++ + ""};
+	ll.addFirst(s2);
+	String[] s3 = {"bob","David","hallo David die dritte",seqCounter++ + ""};
+	ll.addFirst(s3);
+	String[] s4 = {"bob","David","hallo David die vierte",seqCounter++ + ""};
+	ll.addFirst(s4);
+	}
 	/**
 	 * Empfängt Nachrichten im JSON Format und sendet wenn vorhanden neue Nachrichten.
 	 * @return Array von neuen Nachrichten
@@ -25,26 +36,15 @@ public class Messages {
 	@GET
 	@Produces("application/json")
 	public JSONArray receive(@PathParam("user_id") String username, @PathParam("sequenceNumber") int seqRecieved) throws JSONException {
-		System.out.println("aufruf" + username + seqRecieved);
+		System.out.println("aufruf name: " + username + ", seq: " + seqRecieved);
 		JSONArray responseForUser = new JSONArray();
-		LinkedList<String[]> ll = new LinkedList<>();
-		{
-		String[] s = {"bob","David","hallo David","0"};
-		JSONObject jsonMessage = new JSONObject();
-		jsonMessage.put("from", s[0]);
-		jsonMessage.put("to", s[1]);
-		jsonMessage.put("text", s[2]);
-		jsonMessage.put("sequence", s[3]);
 
-		responseForUser.put(jsonMessage);
-		ll.addFirst(s);
-		}
 
-		for (String[] s : ll) {
+		for (int i = ll.size() - 1; i >= 0; i--) {
+			String[] s = ll.get(i);
 			int seqMessage = Integer.parseInt(s[3]);
-			if(username.equals(s[1]) && seqMessage > seqRecieved){
+			if(username.equals(s[0]) && seqMessage >= seqRecieved){
 				try{
-					System.out.println(s);
 					JSONObject jsonMessage = new JSONObject();
 					jsonMessage.put("from", s[0]);
 					jsonMessage.put("to", s[1]);
@@ -52,8 +52,17 @@ public class Messages {
 					jsonMessage.put("sequence", s[3]);
 					responseForUser.put(jsonMessage);
 				} catch (JSONException e){
-					System.out.println("Fehler beim Senden einer Nachricht");
+					System.out.println("Fehler beim Abrufen einer Nachricht");
 				}
+			} else {
+				System.out.println(seqMessage + " ?>= " + seqRecieved);
+				System.out.println(username + " ?= " +s[1]);
+
+			}
+			if(username.equals(s[0]) && seqMessage < seqRecieved){
+				System.out.println("löschen von " + i);
+				ll.remove(i);
+				System.out.println(ll.size());
 			}
 		}
 		return responseForUser;
